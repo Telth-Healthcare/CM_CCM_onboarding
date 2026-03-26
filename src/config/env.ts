27ac9fd7712e97ba2405ba9bf9167f9c9bc8fead
telth <shanmugaraj@mytelth.com>
2026-03-26 15:58:40 +0530

    url change

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://13.126.59.94:8000/'
-// export const baseUrl = "https://unfulminating-amicably-sha.ngrok-free.dev/"
-export const baseUrl = "https://cmccm.vercel.app/"
+export const baseUrl = "https://unfulminating-amicably-sha.ngrok-free.dev/"
+// export const baseUrl = "https://cmccm.vercel.app/"
 
 // export const baseUrl = import.meta.env.VITE_API_BASE_URL;
